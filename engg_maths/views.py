c6504d3aa00cc04be0c9f018530d7fe81d47e07c
palprashant2002 <palprashant234@gmail.com>
2021-11-29 19:34:34 +0530

    first change

--- engg_maths/views.py
+++ engg_maths/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def fetchfunc(req):
+    x=req.POST.get('xname','error')
+    y=req.POST.get('yname','error')
+    x_lst=list(map(int,x.split()))
+    y_lst=list(map(int,y.split()))
+    a=b=0
+    return a,b;
