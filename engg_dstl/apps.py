c72813c2832c3f437b31c4bc2fe9311b92bd684e
JAYANT GUPTA <jayant.gupta.dln@gmail.com>
2021-11-29 15:56:31 +0530

    initial-create project

---
+++ engg_dstl/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EnggDstlConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'engg_dstl'
---
+++ engg_maths/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EnggMathsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'engg_maths'
