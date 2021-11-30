c9be9bd3708baf87cc07960b8d34df46d700eb6f
palprashant2002 <palprashant234@gmail.com>
2021-11-30 16:55:44 +0530

    parabola

--- engg_maths/views.py
+++ engg_maths/views.py
@@ -9,19 +9,26 @@ def fetchfunc(request):
     k=request.POST['yname']
     x_lst=list(map(float,h.split()))
     y_lst=list(map(float,k.split()))
-    soln=calci(x_lst,y_lst)
+    ch=2
+    if(ch==1):     
+        soln,x2_lst,xy_lst=straight(x_lst,y_lst)
+        a,b=soln.keys()
+        A="{0:.4f}".format(soln[a])
+        B="{0:.4f}".format(soln[b])
+        context = {'value_of_a' : A,'value_of_b' : B}
+        print(A,B)
+    elif(ch==2):
+        soln=parabola(x_lst,y_lst)
+        a,b,c=soln.keys()
+        A="{0:.4f}".format(soln[a])
+        B="{0:.4f}".format(soln[b])
+        C="{0:.4f}".format(soln[c])
+        context = {'value_of_a' :("A is", A),'value_of_b' :("B is", B),'value_of_c' :("C is", C)}
+        print(A,B,C)
     print(soln)
-    a,b=soln.keys()
-    A=round(soln[a],4)
-    B=round(soln[b],4)
-    print(A,B)
-    out_sol=[A,B]
-
-    context = {'value_of_a' : A,'value_of_b' : B}
-
     return render(request, 'solution.html', context)
 
-def calci(X,Y):
+def straight(X,Y):
     X_2=list(map(lambda h: h ** 2, X))
     A=np.array(X,dtype=float)
     B=np.array(Y,dtype=float)
@@ -35,5 +42,28 @@ def calci(X,Y):
     eq1=Eq(n*x + sum_x*y - sum_y)
     eq2=Eq(sum_x*x + sum_x2*y - sum_xy)
     dic=solve((eq1,eq2), (x,y))
-    return dic
+    return dic,X_2,X_Y
 
+def parabola(X,Y):
+    x2=list(map(lambda h: h ** 2, X))
+    x3=list(map(lambda h: h ** 3, X))
+    x4=list(map(lambda h: h ** 4, X))
+    A=np.array(X,dtype=float)
+    B=np.array(Y,dtype=float)
+    C=A*A
+    xy=A*B
+    x2y=C*B
+    sum_x=sum(X)
+    sum_y=sum(Y)
+    sum_x2=sum(x2)
+    sum_x3=sum(x3)
+    sum_x4=sum(x4)
+    sum_xy=sum(xy)
+    sum_x2y=sum(x2y)
+    n=len(X)
+    x,y,z = symbols('x y z')
+    eq1=n*x + sum_x*y +sum_x2*z -sum_y
+    eq2=sum_x*x + sum_x2*y +sum_x3*z -sum_xy
+    eq3=sum_x2*x + sum_x3*y +sum_x4*z -sum_x2y
+    dic=solve((eq1,eq2,eq3), (x,y,z))
+    return dic
