b4328a094b5a62ac4ec5a99cf55f9e3ed713fd16
JAYANT GUPTA <jayant.gupta.dln@gmail.com>
2021-11-30 22:47:10 +0530

    table created

--- engg_maths/admin.py
+++ engg_maths/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from engg_maths.models import MathData
 
 # Register your models here.
+admin.site.register(MathData)
--- engg_maths/models.py
+++ engg_maths/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class MathData(models.Model) :
+    x_list = models.CharField(max_length=100)
+    y_list = models.CharField(max_length=100)
+    x2_list = models.CharField(max_length=100)
+    xy_list = models.CharField(max_length=100)
+
