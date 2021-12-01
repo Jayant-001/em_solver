e3196bfa6207a11baf6abafbcda6dc1bf762c15c
palprashant2002 <palprashant234@gmail.com>
2021-12-01 17:26:17 +0530

    dont add now

--- engg_maths/views.py
+++ engg_maths/views.py
@@ -8,22 +8,16 @@ import ast
 
 # Create your views here.
 
-# def solution(request):
-
-#     mas_tab=[[2,3,4,6],[1,4,1,4],[3,2,9,6]]
-#     cont={'tab':mas_tab}
-#     return render(request,'solution.html',cont)
 
 
 def fetchfunc(request):
     h = request.POST['xname']
     k = request.POST['yname']
-    selectedEqn = request.POST['inputData']
-    print(selectedEqn)
+    ch = request.POST['inputData']
+    print(ch)
     x_lst = list(map(float, h.split()))
     y_lst = list(map(float, k.split()))
-    ch = 1
-    if(ch == 1):
+    if(ch == "1"):
         soln, x2_lst, xy_lst = straight(x_lst, y_lst)
         a, b = soln.keys()
         A = "{0:.4f}".format(soln[a])
@@ -57,34 +51,66 @@ def fetchfunc(request):
         #                      x2_list=x2_lst, xy_list=xy_lst)
         # math_data.save()
 
-    elif(ch == 2):
-        soln = parabola(x_lst, y_lst)
+    elif(ch == "2"):
+        soln,x2_lst,x3_lst,x4_lst,xy_lst,x2y_lst= parabola(x_lst, y_lst)
         a, b, c = soln.keys()
         A = "{0:.4f}".format(soln[a])
         B = "{0:.4f}".format(soln[b])
         C = "{0:.4f}".format(soln[c])
-        context = {'value_of_a': ("A is", A), 'value_of_b': (
-            "B is", B), 'value_of_c': ("C is", C)}
+        context = {'value_of_a': ("A is", A), 'value_of_b': ("B is", B), 'value_of_c': ("C is", C)}
         print(A, B, C)
+        master_lst = []
+        for i in range(len(x_lst)):
+            lst = []
+            lst.append(x_lst[i])
+            lst.append(y_lst[i])
+            lst.append(x2_lst[i])
+            lst.append(x3_lst[i])
+            lst.append(x4_lst[i])
+            lst.append(xy_lst[i])
+            lst.append(x2y_lst[i])
+            master_lst.append(lst)
+        lst = [sum(x_lst), sum(y_lst),sum(x2_lst), sum(x3_lst),sum(x4_lst), sum(xy_lst),sum(x2y_lst)]
+        master_lst.append(lst)
+        context['tab'] = master_lst
+    elif(ch=="3"):
+        soln,Y_lst,x2_lst,xy_lst=exp(x_lst,y_lst)
+        a, b = soln.keys()
+        A = "{0:.4f}".format(soln[a])
+        B = "{0:.4f}".format(soln[b])
+        context = {'value_of_a': A, 'value_of_b': B}
+        print(A, B)
+        master_lst = []
+        for i in range(len(x_lst)):
+            lst = []
+            lst.append(x_lst[i])
+            lst.append(y_lst[i])
+            lst.append(Y_lst[i])
+            lst.append(x2_lst[i])
+            lst.append(xy_lst[i])
+            master_lst.append(lst)
+        lst = [sum(x_lst), sum(y_lst),sum(Y_lst), sum(x2_lst), sum(xy_lst)]
+        master_lst.append(lst)
+        context['tab'] = master_lst
     print(soln)
     return render(request, 'solution.html', context)
 
 
 def straight(X, Y):
-    X_2 = list(map(lambda h: h ** 2, X))
+    x2 = list(map(lambda h: h ** 2, X))
     A = np.array(X, dtype=float)
     B = np.array(Y, dtype=float)
-    X_Y = A*B
+    xy = A*B
     sum_x = sum(X)
     sum_y = sum(Y)
-    sum_x2 = sum(X_2)
-    sum_xy = sum(X_Y)
+    sum_x2 = sum(x2)
+    sum_xy = sum(xy)
     n = len(X)
     x, y = symbols('x y')
     eq1 = Eq(n*x + sum_x*y - sum_y)
     eq2 = Eq(sum_x*x + sum_x2*y - sum_xy)
     dic = solve((eq1, eq2), (x, y))
-    return dic, X_2, X_Y
+    return dic, x2, xy
 
 
 def parabola(X, Y):
@@ -109,18 +135,23 @@ def parabola(X, Y):
     eq2 = sum_x*x + sum_x2*y + sum_x3*z - sum_xy
     eq3 = sum_x2*x + sum_x3*y + sum_x4*z - sum_x2y
     dic = solve((eq1, eq2, eq3), (x, y, z))
-    return dic
+    return dic,x2,x3,x4,xy,x2y
 
 
 def exp(X,Y):
     y=list(map(lambda h: log10(h),Y))
-    
-    # x2 = list(map(lambda h: h ** 2, X))
-    # A = np.array(X, dtype=float)
-    # B = np.array(y, dtype=float)
-    # C = A*A
-    # xy = A*B
-    # sum_x = sum(X)
-    # sum_y = sum(y)
-    # sum_x2 = sum(x2)
-    # sum_xy = sum(xy)
+    x2 = list(map(lambda h: h ** 2, X))
+    A = np.array(X, dtype=float)
+    B = np.array(y, dtype=float)
+    C = A*A
+    xy = A*B
+    sum_x = sum(X)
+    sum_y = sum(y)
+    sum_x2 = sum(x2)
+    sum_xy = sum(xy)
+    n = len(X)
+    x, y = symbols('x y')
+    eq1 = Eq(n*x + sum_x*y - sum_y)
+    eq2 = Eq(sum_x*x + sum_x2*y - sum_xy)
+    dic = solve((eq1, eq2), (x, y))
+    return dic, y,x2,xy;
